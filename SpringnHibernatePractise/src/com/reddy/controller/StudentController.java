4986b2fe772298cd65b6e62f6559631a4820c5d6
Anil-Home <Anil-Home@Anil>
2015-07-08 23:07:53 +0530

    this is my 3rd commit msg

--- SpringnHibernatePractise/src/com/reddy/controller/StudentController.java
+++ SpringnHibernatePractise/src/com/reddy/controller/StudentController.java
@@ -5,6 +5,7 @@ public static void main(String[] args) {
 	System.out.println("StudentController.main()");
 	System.out.println("StudentController()");
 	System.out.println("StudentController.main()");
+	System.out.println("StudentController.main()");
 
 }
 
