398f40aaf3c5990e730a1b983a1093f68705ede0
Anil-Home <Anil-Home@Anil>
2015-07-08 23:07:08 +0530

    this is my second commit...

--- SpringnHibernatePractise/src/com/reddy/controller/StudentController.java
+++ SpringnHibernatePractise/src/com/reddy/controller/StudentController.java
@@ -3,5 +3,9 @@ package com.reddy.controller;
 public class StudentController {
 public static void main(String[] args) {
 	System.out.println("StudentController.main()");
+	System.out.println("StudentController()");
+	System.out.println("StudentController.main()");
+
 }
+
 }
